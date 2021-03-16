dfcab059ea9d30167bd73bb6b0470582f7df6d88
Cory Groom <corypgr@gmail.com>
2021-03-15 17:55:07 -0700

    Problem 1 solution

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# project-euler
-Personal files for solving Project Euler problems
+I've been thinking about revisiting [Project Euler](https://projecteuler.net) for a while now, and I've decided to actually track my code this time around.
+
+This repo is for my code solutions to the Project Euler problems. I've worked on this at least twice before, and either can't find the code from then (read: didn't try to find) or the other attempts were shared with someone. This time I thought I would start fresh.
+
+My goal to to finish at least 1 problem each day. Project Euler gives a nice graphic to show how many problems you've solved. I've apparently already solved 80 problems, so I'll focus on those problems first so that my graphic eventually matches my progress here:
+
+![Project Euler Progress](https://projecteuler.net/profile/corypgr.png)
---
+++ src/main/java/PE0001.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+/**
+ * Problem 1
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * The classic problem of determining the multiples of 3 and 5. The easy
+ * solution is to just check every number below 1000 to see if it is divisible
+ * by 3 or 5. We can be more efficient by generating all of these numbers
+ * directly using addition and handling collisions. Since we're dealing with a
+ * pretty small number of elements I think dumping all of the multiples of 3
+ * and 5 into a HashSet and then adding the numbers up works well. If we had a
+ * particularly large set of numbers to compute we could use a bit vector or
+ * some other optimization to avoid the large memory footprint.
+ */
+public class PE0001 {
+    public static void main(String[] args) {
+        HashSet<Integer> multiples = new HashSet<Integer>();
+
+        for (int i = 3; i < 1000; i += 3) {
+            multiples.add(i);
+        }
+
+        for (int i = 5; i < 1000; i += 5) {
+            multiples.add(i);
+        }
+
+        int sum = multiples.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Sum: " + sum);
+    }
+}
