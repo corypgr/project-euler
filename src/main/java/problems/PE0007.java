db9b6abb799e5809213b7c0c1b9e49271d099fc3
Cory Groom <corypgr@gmail.com>
2021-03-16 11:31:40 -0700

    Problem 7 solution

---
+++ src/main/java/problems/PE0007.java
@@ -0,0 +1,34 @@
+package problems;
+
+import prime.PrimeGenerator;
+
+import java.util.List;
+
+/**
+ * Problem 7
+ *
+ * https://projecteuler.net/problem=7
+ *
+ * This is a little interesting. I already have something to generate prime numbers, but it relies on knowing the max
+ * prime number to generate the list. I can't think of a great way to modify that algorithm to give us a specific prime
+ * number count. Instead, let's do some exponential increase until we hit the size we're looking for.
+ */
+public class PE0007 {
+    // Position is 10001 - 1 to account for zero indexing.
+    private static final int PRIME_POSITION = 10000;
+
+    public static void main(String[] args) {
+        PrimeGenerator primeGenerator = new PrimeGenerator();
+
+        // Start at something reasonable compared to the size we're looking for.
+        // Every second and every third value is not prime, so try 3X to start.
+        long maxVal = PRIME_POSITION * 3;
+        List<Long> primes = primeGenerator.generatePrimesList(maxVal);
+        while (primes.size() < PRIME_POSITION) {
+            maxVal *= 2;
+            primes = primeGenerator.generatePrimesList(maxVal);
+        }
+
+        System.out.println("10001st prime: " + primes.get(PRIME_POSITION));
+    }
+}
