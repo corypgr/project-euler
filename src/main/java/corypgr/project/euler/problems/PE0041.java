aada28d030cf721560842462ea7cd38d1bf51a14
Cory Groom <corypgr@gmail.com>
2021-04-05 15:13:44 -0700

    Improve Problem 41 runtime

--- src/main/java/corypgr/project/euler/problems/PE0041.java
+++ src/main/java/corypgr/project/euler/problems/PE0041.java
@@ -1,6 +1,7 @@
 package corypgr.project.euler.problems;
 
 import corypgr.project.euler.problems.prime.PrimeGenerator;
+import corypgr.project.euler.problems.util.PermutationUtil;
 import corypgr.project.euler.problems.util.Problem;
 import corypgr.project.euler.problems.util.ProblemSolution;
 
@@ -22,18 +23,39 @@ import java.util.stream.Collectors;
  * Cheating a little bit here. 987654321 and 87654321 both resulted in OutOfMemory errors :). When running with 7654321
  * as the max we get the result we're looking for. Still takes 5-6 seconds to run everything, but I can't get that
  * shorter without figuring out a more efficient way to generate the large prime numbers.
+ *
+ * --------
+ * Read through some of the solutions in the Project Euler thread on this. Found some interesting ideas to speed it up.
+ * First, we can see that 8+7+6+5+4+3+2+1 = 36. This is divisible by 3, so we know that the answer isn't 8 digits long.
+ * Similarly with 9 digits, the sum is 45, also divisible by 3.
+ *
+ * Since we're trying to find the largest pandigital prime, we can calculate things in a different order. We can go back
+ * from 7654321, and for every pandigital number, check if it is prime. We can generate all permutations of the 7 digits
+ * and sort them to get all pandigital numbers. To check if the number is prime, we check if it is divisible by any of
+ * the primes under sqrt(7654321L), which we can generate up front.
+ *
+ * This gets the runtime under 100 ms.
  */
 public class PE0041 implements Problem {
+    private static final List<Integer> DIGITS = List.of(1, 2, 3, 4, 5, 6, 7);
     private static final long MAX_PANDIGITAL = 7654321L;
+
     @Override
     public ProblemSolution solve() {
+        PermutationUtil<Integer> permutationUtil = new PermutationUtil<>();
+        List<List<Integer>> permutations = permutationUtil.getAllPermutations(DIGITS);
+        List<Long> pandigitalsSorted = permutations.stream()
+                .map(this::permutationToLong)
+                .sorted()
+                .collect(Collectors.toList());
+
         PrimeGenerator primeGenerator = new PrimeGenerator();
-        List<Long> primes = primeGenerator.generatePrimesList(MAX_PANDIGITAL);
+        Set<Long> primes = primeGenerator.generatePrimesSet((long) Math.sqrt(MAX_PANDIGITAL));
 
         long largestPandigitalPrime = -1L;
-        for (int i = primes.size() - 1; largestPandigitalPrime < 0; i--) {
-            if (isPandigital(primes.get(i))) {
-                largestPandigitalPrime = primes.get(i);
+        for (int i = pandigitalsSorted.size() - 1; largestPandigitalPrime < 0; i--) {
+            if (isPrime(pandigitalsSorted.get(i), primes)) {
+                largestPandigitalPrime = pandigitalsSorted.get(i);
             }
         }
 
@@ -43,16 +65,21 @@ public class PE0041 implements Problem {
                 .build();
     }
 
-    private boolean isPandigital(long prime) {
-        String primeStr = String.valueOf(prime);
-        int primeStrLength = primeStr.length();
+    private long permutationToLong(List<Integer> permutation) {
+        long result = 0;
+        for (int i = permutation.size() - 1; i >= 0; i--) {
+            result *= 10;
+            result += permutation.get(i);
+        }
+        return result;
+    }
 
-        Set<Integer> uniqueDigits = primeStr.chars()
-                .mapToObj(Character::getNumericValue)
-                .filter(v -> v <= primeStrLength)
-                .collect(Collectors.toSet());
-
-        // 0 is not a number in the pandigital sequence.
-        return uniqueDigits.size() == primeStr.length() && !uniqueDigits.contains(0);
+    private boolean isPrime(long val, Set<Long> primes) {
+        for (long prime : primes) {
+            if (val % prime == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
