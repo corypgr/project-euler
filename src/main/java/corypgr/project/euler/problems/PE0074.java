8361e860b9ad247ccda4fb99795d66120c63830e
Cory Groom <corypgr@gmail.com>
2021-05-14 14:58:02 -0700

    Improve Problem 74 solution

--- src/main/java/corypgr/project/euler/problems/PE0074.java
+++ src/main/java/corypgr/project/euler/problems/PE0074.java
@@ -24,6 +24,11 @@ import java.util.stream.LongStream;
  * Next idea, create linked lists for each chain, with a mapping from the value to the node in the chain. When we see a
  * specific value that we've already processed, we can simply drop into the chain we've already computed and walk the
  * nodes until we see repeats. This would at least save us recomputing the chain values.
+ *
+ * The above works, running in around 2-3 seconds. Reading the problem more closely, I see that I was incorrect in my
+ * earlier musing. We do know when the chain loops. The problem statement tells us the only 3 non-self loops. Given
+ * that information, and a check for self-loops, we do a recursive solution that memoizes the length of the chain. This
+ * brings us well under a 1 second runtime.
  */
 public class PE0074 implements Problem {
     private static final long MAX_NUM = 1_000_000;
@@ -40,10 +45,20 @@ public class PE0074 implements Problem {
 
     @Override
     public ProblemSolution solve() {
-        Map<Long, Node> valToNode = new HashMap<>();
+        Map<Long, Integer> valToChainLength = new HashMap<>();
+        // 169 -> 363601 -> 1454 -> 169 loop
+        valToChainLength.put(169L, 3);
+        valToChainLength.put(363601L, 3);
+        valToChainLength.put(1454L, 3);
+        // 871 -> 45361 -> 871 loop
+        valToChainLength.put(871L, 2);
+        valToChainLength.put(45361L, 2);
+        // 872 -> 45362 -> 872 loop
+        valToChainLength.put(872L, 2);
+        valToChainLength.put(45362L, 2);
 
         long countOfChainsAtTargetLength = LongStream.range(1, MAX_NUM)
-                .map(v -> getNonRepeatingLength(v, valToNode))
+                .map(v -> getNonRepeatingLength(v, valToChainLength))
                 .filter(v -> v == TARGET_CHAIN_LENGTH)
                 .count();
         return ProblemSolution.builder()
@@ -52,36 +67,21 @@ public class PE0074 implements Problem {
                 .build();
     }
 
-    private long getNonRepeatingLength(long value, Map<Long, Node> valToNode) {
-        Node node = connectChain(value, valToNode);
-        Set<Long> seenVals = new HashSet<>();
-        while (!seenVals.contains(node.getValue())) {
-            seenVals.add(node.getValue());
-            node = node.getNext();
-        }
-        return seenVals.size();
-    }
-
-    private Node connectChain(long value, Map<Long, Node> valToNode) {
-        if (valToNode.containsKey(value)) {
-            valToNode.get(value);
+    private int getNonRepeatingLength(long value, Map<Long, Integer> valToChainLength) {
+        int memoizedChainLength = valToChainLength.getOrDefault(value, -1);
+        if (memoizedChainLength > -1) {
+            return memoizedChainLength;
         }
 
-        Node startNode = new Node(value);
-        valToNode.put(value, startNode);
-        Node prevNode = startNode;
-        long nextVal = getNextValue(value);
-        while (!valToNode.containsKey(nextVal)) {
-            Node nextNode = new Node(nextVal);
-            valToNode.put(nextVal, nextNode);
-            prevNode.setNext(nextNode);
-
-            prevNode = nextNode;
-            nextVal = getNextValue(nextVal);
+        long nextValue = getNextValue(value);
+        if (nextValue == value) {
+            valToChainLength.put(value, 1);
+            return 1;
         }
-        prevNode.setNext(valToNode.get(nextVal));
 
-        return startNode;
+        int lengthResult = getNonRepeatingLength(nextValue, valToChainLength) + 1;
+        valToChainLength.put(value, lengthResult);
+        return lengthResult;
     }
 
     private long getNextValue(long value) {
@@ -93,13 +93,5 @@ public class PE0074 implements Problem {
             remaining /= 10;
         }
         return result;
-    }
-
-    @Data
-    private static final class Node {
-        private final long value;
-
-        @Setter
-        private Node next;
     }
 }
