b9c3e25bf895b28302f00275783bfae933185c38
Cory Groom <corypgr@gmail.com>
2021-05-22 16:59:38 -0700

    Update Problem 3 to use common DivisorsUtil

--- src/main/java/corypgr/project/euler/problems/PE0003.java
+++ src/main/java/corypgr/project/euler/problems/PE0003.java
@@ -1,10 +1,10 @@
 package corypgr.project.euler.problems;
 
-import corypgr.project.euler.problems.prime.PrimeGenerator;
+import corypgr.project.euler.problems.util.DivisorsUtil;
 import corypgr.project.euler.problems.util.Problem;
 import corypgr.project.euler.problems.util.ProblemSolution;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * Problem 3
@@ -13,30 +13,26 @@ import java.util.List;
  *
  * So we need to find the Prime factors of this number, which means we need to know what numbers are Prime. After
  * knowing what numbers are prime, the rest is pretty straightforward.
+ * ---------
+ * At some point I wrote a general utility for finding prime divisors. Swapping that in here. My old solution here is
+ * actually not quite correct. It only checks for prime numbers <= sqrt(TARGET). In the general case you can have a
+ * prime divisor that is larger than sqrt(TARGET).
  */
 public class PE0003 implements Problem {
     private static final long TARGET = 600851475143L;
 
     @Override
     public ProblemSolution solve() {
-        PrimeGenerator primeGenerator = new PrimeGenerator();
+        DivisorsUtil divisorsUtil = new DivisorsUtil();
+        Set<Long> primeDivisors = divisorsUtil.getPrimeDivisors(TARGET);
+        long largestPrimeDivisor = primeDivisors.stream()
+                .mapToLong(Long::longValue)
+                .max()
+                .getAsLong();
 
-        // Use the square root as the max value since any prime factor must be <= the square root.
-        List<Long> primes = primeGenerator.generatePrimesList((long) Math.sqrt(TARGET));
-
-        // Check in reverse order since we're looking for the largest value.
-        for (int i = primes.size() - 1; i >= 0; i--) {
-            long prime = primes.get(i);
-            if (TARGET % prime == 0) {
-                return ProblemSolution.builder()
-                        .solution(prime)
-                        .descriptiveSolution("Largest Prime Factor: " + prime)
-                        .build();
-            }
-        }
         return ProblemSolution.builder()
-                .solution(TARGET)
-                .descriptiveSolution("No Other Prime factors found. " + TARGET + " must be prime.")
+                .solution(largestPrimeDivisor)
+                .descriptiveSolution("Largest Prime Factor: " + largestPrimeDivisor)
                 .build();
     }
 }
